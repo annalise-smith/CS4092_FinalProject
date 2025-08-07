843620e57822493f784111857e25a060cb476c1f
Annalise Smith <smit7am@mail.uc.edu>
2025-08-07 01:08:26 -0400

    Seeing if datatbases are connected

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#test to see if I know what I am doing
+import mysql.connector
+from datetime import date
+
+try:
+    db = mysql.connector.connect(
+        host="localhost",
+        user="annalise.smith05@gmail.com",
+        password="TemporaryForCla$$1",
+        database="ecommerce"
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SHOW TABLES;")
+    print("Connected successfully. Tables in database:")
+    for table in cursor:
+        print(table)
+
+except mysql.connector.Error as err:
+    print("Connection error:", err)
