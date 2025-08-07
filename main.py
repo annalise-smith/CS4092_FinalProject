d80806262234343efa0981ee8658ffb60d5a9da8
Annalise Smith <smit7am@mail.uc.edu>
2025-08-07 01:56:29 -0400

    trying to debug

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import mysql.connector
 from datetime import date
 
+print("Script started")
 try:
+    print("Trying to connect to database...")
     db = mysql.connector.connect(
         host="localhost",
         user="annalise.smith05@gmail.com",
         password="TemporaryForCla$$1",
         database="ecommerce"
     )
+    print("Connected!")
 
     cursor = db.cursor()
     cursor.execute("SHOW TABLES;")
@@ -17,3 +20,6 @@ try:
 
 except mysql.connector.Error as err:
     print("Connection error:", err)
+
+except Exception as e:
+    print("Unexpected error:", e)
